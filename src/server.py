18dcb71a5a15b86209cd27235016316223332f1d
Aleksandr Sichkar <aleksandrsichkar@Aleksandrs-MBP.home>
2023-05-28 18:42:28 +0200

    Added tests; updated requirements for pip

---
+++ src/server.py
@@ -0,0 +1,101 @@
+from pydantic import ValidationError
+import uvicorn
+from fastapi import FastAPI, WebSocket, status
+from fastapi.responses import JSONResponse
+from src.order import Order, retrieve_order
+
+app = FastAPI()
+
+active_connections = []
+orders_db = []
+
+
+@app.post("/orders", response_model=dict)
+async def create_order(orderBody: dict):
+    exception = None
+
+    try:
+        new_order = Order(
+            stocks=orderBody["stocks"], quantity=orderBody["quantity"], status="PENDING"
+        )
+    except ValidationError as e:
+        exception = give_error(f"{e.raw_errors[0]._loc} value is invalid", 10)
+    except KeyError as e:
+        exception = give_error(f"{e.args[0]} value is missing", 20)
+
+    if exception:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content=exception,
+        )
+
+    orders_db.append(new_order)
+
+    response_body = {"id": new_order.id, "status": new_order.status}
+
+    await broadcast_over_websocket(
+        f"Order {new_order.id} for {new_order.stocks} received. Current status - {new_order.status}"
+    )
+
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=response_body)
+
+
+@app.get("/orders/{order_id}")
+async def read_item(order_id: str) -> dict:
+    order = retrieve_order(order_id=order_id, order_list=orders_db)
+
+    if order:
+        response_body = {"stocks": order.stocks, "quantity": order.quantity}
+        response_code = status.HTTP_200_OK
+    else:
+        response_body = give_error(f"order {order_id} not found", 40)
+        response_code = status.HTTP_404_NOT_FOUND
+
+    return JSONResponse(status_code=response_code, content=response_body)
+
+
+@app.get("/orders")
+async def read_item() -> list:
+    return orders_db
+
+
+@app.delete("/orders/{order_id}")
+async def delete_order(order_id: str) -> dict:
+    order = retrieve_order(order_id=order_id, order_list=orders_db, delete=True)
+
+    if order:
+        response_body = None
+        response_code = status.HTTP_204_NO_CONTENT
+    else:
+        response_body = give_error(f"Order {order_id} not found", 40)
+        response_code = status.HTTP_404_NOT_FOUND
+
+    return JSONResponse(status_code=response_code, content=response_body)
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    active_connections.append(websocket)
+    await websocket.accept()
+    while True:
+        # think about keep-alive mechanism
+        await websocket.receive_text()
+
+
+async def broadcast_over_websocket(message: str):
+    if len(active_connections) > 0:
+        for connection in active_connections:
+            await connection.send_text(message)
+    else:
+        print("No websocket connected; skipping update")
+
+
+def give_error(message: str, code: int) -> dict:
+    return {"message": message, "code": code}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
+
+
+# USD USD currency pair can not exist
---
+++ test/test_api.py
@@ -0,0 +1,67 @@
+import pytest
+from fastapi.testclient import TestClient
+from src.server import app
+
+client = TestClient(app)
+
+
+class DummyOrder:
+    def __init__(self, id, quantity, status):
+        self.id = id
+        self.quantity = quantity
+        self.stocks = "USDUSD"
+        self.status = status
+
+
+@pytest.mark.parametrize(
+    "test_body,expected_response_code",
+    [
+        ({"stocks": "GBPUSD", "quantity": 123}, 201),
+        ({"stocks": "GBPUSD", "quantity": "invalid_qty"}, 400),
+        ({"stocks": "invalid_stock", "quantity": 123}, 400),
+        ({"stocks": "invalid_stock"}, 400),
+        ({"stocks": "NNNUSD", "quantity": 123}, 400),
+    ],
+)
+def test_create_order(test_body, expected_response_code):
+    test_request = client.post("/orders", json=test_body)
+    assert test_request.status_code == expected_response_code
+
+
+def test_get_orders(mocker):
+    test_order_db = ["order_1", "order_2"]
+    mocker.patch("src.server.orders_db", test_order_db)
+    test_request = client.get("/orders")
+    assert test_request.status_code == 200
+    assert test_request.json() == test_order_db
+
+
+@pytest.mark.parametrize(
+    "order_id, order_id_target, status_code",
+    [
+        ("UUID1111", "UUID1111", 200),
+        ("UUID1111", "UUID2222", 404),
+    ],
+)
+def test_get_order(mocker, order_id, order_id_target, status_code):
+    test_order_db = [DummyOrder(id=order_id, quantity=10, status="PENDING")]
+    mocker.patch("src.server.orders_db", test_order_db)
+    test_request = client.get(f"/orders/{order_id_target}")
+
+    assert test_request.status_code == status_code
+
+
+@pytest.mark.parametrize(
+    "order_id, order_id_target, status_code, final_status",
+    [
+        ("UUID1111", "UUID1111", 204, "CANCELLED"),
+        ("UUID1111", "UUID2222", 404, "PENDING"),
+    ],
+)
+def test_delete_order(mocker, order_id, order_id_target, status_code, final_status):
+    test_order_db = [DummyOrder(id=order_id, quantity=10, status="PENDING")]
+    mocker.patch("src.server.orders_db", test_order_db)
+    test_request = client.delete(f"/orders/{order_id_target}")
+
+    assert test_request.status_code == status_code
+    assert test_order_db[0].status == final_status
